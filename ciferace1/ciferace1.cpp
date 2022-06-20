fafeda0db4a96fbda7ec3281457c32e78515d2c7
matous.bien.020 <matous.bien.020@B000-10>
2022-06-20 14:07:52 +0200

    Přidejte soubory projektu.

---
+++ ciferace1/ciferace1.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x;
+	int soucet = 0;
+	int i;
+	printf("Zadejte cislo: ");
+	scanf_s("%d", &x);
+	for (i = 0; x > 0; i++);
+	{
+		i = x % 10;
+		x = x / 10;
+		soucet = soucet + i;
+	}
+	int y = 10;
+	if (soucet >= 10) {
+		for (i = 0; y > 0; i++)
+		{
+			i = y % 10;
+			y = y / 10;
+			soucet = soucet + i;
+			printf("%d", soucet);
+		}
+	}
+	else {
+		printf("%d", soucet);
+	}
+	return 0;
+
+
+}
+
